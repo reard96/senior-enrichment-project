c94599a0ff1c51aae8d5fbfc41ef7d59b857ccad
reard96 <reardon.danielj@gmail.com>
2018-04-07 16:25:32 -0400

    most of store added; functionality broken

--- src/Student.js
+++ src/Student.js
@@ -1,14 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { saveStudent, deleteStudent } from './store';
 
-const Student = ({ student }) => {
-  if (!student) {
-    return null;
+class Student extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: this.props.student ? this.props.student.firstName : '',
+      lastName: this.props.student ? this.props.student.lastName : '',
+      email: this.props.student ? this.props.student.email : '',
+      gpa: this.props.student ? this.props.student.gpa : 0.00,
+    };
+
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeGpa = this.onChangeGpa.bind(this);
+    this.onSave = this.onSave.bind(this);
+    this.onDelete = this.onDelete.bind(this);
   }
-  return (
-    <h1>{ student.name }</h1>
-  );
-};
+
+  onChangeFirstName(ev) {
+    this.setState({ firstName: ev.target.value });
+  }
+
+  onChangeLastName(ev) {
+    this.setState({ lastName: ev.target.value });
+  }
+
+  onChangeEmail(ev) {
+    this.setState({ email: ev.target.value });
+  }
+
+  onChangeGpa(ev) {
+    this.setState({ gpa: ev.target.value });
+  }
+
+  onSave(ev) {
+    ev.preventDefault();
+    const student = { id: this.props.id,
+                      firstName: this.state.firstName,
+                      lastName: this.state.lastName,
+                      email: this.state.email,
+                      gpa: this.state.gpa };
+
+    this.props.saveStudent(student);
+  }
+
+  onDelete() {
+    this.props.deleteStudent({ id: this.props.id });
+  }
+
+  render() {
+    const { student } = this.props;
+    const { firstName, lastName, email, gpa }  = this.state;
+    const { onChangeFirstName, onChangeLastName, onChangeEmail, onChangeGpa, onSave, onDelete } = this;
+
+    if (!student) {
+      return <div>No student found!</div>;
+    }
+    return (
+      <div>
+        <h1>{ student.name }</h1>
+        <form onSubmit={ onSave }>
+          <input type="text" value={ firstName } onChange={ onChangeFirstName } />
+          <input type="text" value={ lastName } onChange={ onChangeLastName } />
+          <input type="email" value={ email } onChange={ onChangeEmail } />
+          <input type="number" value={ gpa } onChange={ onChangeGpa } />
+          <button className="btn btn-primary">Update Student</button>
+        </form>
+        <button className="btn btn-danger" onClick={ onDelete }>Delete</button>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({ students }, { id }) => {
   const student = students.find(item => item.id === id);
@@ -17,4 +82,11 @@ const mapStateToProps = ({ students }, { id }) => {
   };
 };
 
-export default connect(mapStateToProps)(Student);
+const mapDispatchToProps = (dispatch, { history }) => {
+  return {
+    saveStudent: (student) => dispatch(saveStudent(student, history)),
+    deleteStudent: (student) => dispatch(deleteStudent(student, history))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Student);
--- src/store.js
+++ src/store.js
@@ -1,28 +1,21 @@
+/* eslint-disable */
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import axios from 'axios';
 
+// STUDENT ACTION TYPES
 const SET_STUDENTS = 'SET_STUDENTS';
+const UPDATE_STUDENT = 'UPDATE_STUDENT';
+const DELETE_STUDENT = 'DELETE_STUDENT';
+const CREATE_STUDENT  = 'CREATE_STUDENT';
+
+// CAMPUS ACTION TYPES
 const SET_CAMPUSES = 'SET_CAMPUSES';
+const UPDATE_CAMPUS = 'UPDATE_CAMPUS';
+const DELETE_CAMPUS = 'DELETE_CAMPUS';
+const CREATE_CAMPUS  = 'CREATE_CAMPUS';
 
-const studentsReducer = (state = [], action) => {
-  switch(action.type) {
-    case SET_STUDENTS:
-      state = action.students;
-      break;
-  }
-  return state;
-};
-
-const campusReducer = (state = [], action) => {
-  switch(action.type) {
-    case SET_CAMPUSES:
-      state = action.campuses;
-      break;
-  }
-  return state;
-};
-
+// STUDENT ACTION CREATORS
 const loadStudents = () => {
   return (dispatch) => {
     return axios.get('/api/students')
@@ -35,6 +28,51 @@ const loadStudents = () => {
   };
 };
 
+const saveStudent = (student, history) => {
+  if (student.id) {
+    return (dispatch) => {
+      return axios.put(`api/students/${ student.id }`, student)
+        .then(result => result.data)
+        .then(student => dispatch({
+          type: UPDATE_STUDENT,
+          student
+        })
+      )
+      .then(() => {
+        history.push('/api/students');
+      });
+    };
+  }
+  return (dispatch) => {
+    return axios.post('api/students', student)
+      .then(result => result.data)
+      .then(student => dispatch({
+        type: CREATE_STUDENT,
+        student
+      })
+    )
+    .then(() => {
+      history.push('/api/students');
+    });
+  };
+};
+
+const deleteStudent = (student, history) => {
+  return (dispatch) => {
+    return axios.delete(`api/students/${ student.id }`)
+      .then(result => result.data)
+      .then(() => dispatch({
+        type: DELETE_STUDENT,
+        student
+      })
+    )
+    .then(() => {
+      history.push('/api/students');
+    });
+  };
+};
+
+// CAMPUS ACTION CREATORS
 const loadCampuses = () => {
   return (dispatch) => {
     return axios.get('/api/campuses')
@@ -47,6 +85,87 @@ const loadCampuses = () => {
   };
 };
 
+const saveCampus = (campus, history) => {
+  if (campus.id) {
+    return (dispatch) => {
+      return axios.put(`api/campuses/${ campus.id }`, campus)
+        .then(result => result.data)
+        .then(campus => dispatch({
+          type: UPDATE_CAMPUS,
+          campus
+        })
+      )
+      .then(() => {
+        history.push('/api/campuses');
+      });
+    };
+  }
+  return (dispatch) => {
+    return axios.post('api/campuses', campus)
+      .then(result => result.data)
+      .then(campus => dispatch({
+        type: CREATE_CAMPUS,
+        campus
+      })
+    )
+    .then(() => {
+      history.push('/api/campuses');
+    });
+  };
+};
+
+const deleteCampus = (campus, history) => {
+  return (dispatch) => {
+    return axios.delete(`api/campuses/${ campus.id }`)
+      .then(result => result.data)
+      .then(() => dispatch({
+        type: DELETE_CAMPUS,
+        campus
+      })
+    )
+    .then(() => {
+      history.push('/api/campuses');
+    });
+  };
+};
+
+// REDUCERS
+const studentsReducer = (state = [], action) => {
+  switch(action.type) {
+    case SET_STUDENTS:
+      state = action.students;
+      break;
+    case UPDATE_STUDENT:
+      state = state.map(student => student.id === action.student.id ? action.student : student);
+      break;
+    case DELETE_STUDENT:
+      state = state.filter(student => student.id !== action.student.id ? action.student : student);
+      break;
+    case CREATE_STUDENT:
+      state = [...state, action.student];
+      break;
+  }
+  return state;
+};
+
+const campusReducer = (state = [], action) => {
+  switch(action.type) {
+    case SET_CAMPUSES:
+      state = action.campuses;
+      break;
+      case UPDATE_CAMPUS:
+      state = state.map(campus => campus.id === action.campus.id ? action.campus : campus);
+      break;
+    case DELETE_CAMPUS:
+      state = state.filter(campus => campus.id !== action.campus.id ? action.campus : campus);
+      break;
+    case CREATE_CAMPUS:
+      state = [...state, action.campus];
+      break;
+  }
+  return state;
+};
+
 const reducer = combineReducers({
   students: studentsReducer,
   campuses: campusReducer
@@ -56,4 +175,4 @@ const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
 
-export { loadStudents, loadCampuses };
+export { loadStudents, loadCampuses, saveStudent, deleteStudent, saveCampus, deleteCampus };
