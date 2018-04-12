76421f7f6efe54f60637dc1526ed446f74a04a52
Dan Reardon <reardon.danielj@gmail.com>
2018-04-12 15:34:55 -0400

    set up style modules

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink, withRouter } from 'react-router-dom';
 
+import style from '../styles/nav.css';
+
 const Nav = ({ students, campuses }) => {
   return (
     <ul>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink className={ style.linkActive } to="/">Home</NavLink>
       </li>
       <li>
         <NavLink to="/students">Students: { students.length }</NavLink>
