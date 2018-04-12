f2b1dfbd63499ba6b16ed4b7e8d26019513fab5b
Dan Reardon <reardon.danielj@gmail.com>
2018-04-12 14:32:28 -0400

    organize server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const db = require('./db');
+const db = require('./server/db');
 
 app.use(require('body-parser').json());
 
@@ -11,8 +11,8 @@ app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 // Routing
-app.use('/api/students', require('./routes/students'));
-app.use('/api/campuses', require('./routes/campuses'));
+app.use('/api/students', require('./server/routes/students'));
+app.use('/api/campuses', require('./server/routes/campuses'));
 
 const port = process.env.PORT || 3000;
 
