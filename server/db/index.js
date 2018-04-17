6b8c744736bb3973456748290cd009a55654a3f5
reard96 <reardon.danielj@gmail.com>
2018-04-17 08:45:04 -0400

    functional seed

--- server/db/index.js
+++ server/db/index.js
@@ -1,32 +1,81 @@
 const conn = require('./conn');
 const Student = require('./Student');
 const Campus = require('./Campus');
+const Promise = require('bluebird');
+const chance = require('chance')(123);
 
 // define relationships
 Student.belongsTo(Campus);
 Campus.hasMany(Student);
 
-const seedStudent = [
-  { firstName: 'Dan', lastName: 'Reardon', email: 'dan@penn.edu', campusId: 1},
-  { firstName: 'Greg', lastName: 'Moon', email: 'greg@nyu.edu', campusId: 1 },
-  { firstName: 'Fendi', lastName: 'Liu', email: 'fendi@columbia.edu', campusId: 1 }
-];
+const numStudents = 500;
+const numCampuses = 20;
 
-const seedCampus = [
-  { name: 'NYU' },
-  { name: 'Columbia' },
-  { name: 'Penn' },
-  { name: 'Princeton' }
-];
+function doTimes (n, fn) {
+  const results = [];
+  while (n--) {
+    results.push(fn());
+  }
+  return results;
+}
+
+const randStudent = () => {
+  return Student.build({
+    firstName: chance.first(),
+    lastName: chance.last(),
+    email: chance.email()
+  });
+};
+
+const randCampus = () => {
+  return Campus.build({
+    name: chance.word(),
+    description: chance.paragraph()
+  });
+};
+
+const generateStudents = () => {
+  const students = doTimes(numStudents, randStudent);
+  students.push(Student.build({
+    firstName: 'Dan',
+    lastName: 'Reardon',
+    email: 'dan@dan.com'
+  }));
+  return students;
+};
+
+const generateCampuses = () => {
+  const campuses = doTimes(numCampuses, randCampus);
+  campuses.push(Campus.build({
+    name: 'NYU'
+  }));
+  campuses.push(Campus.build({
+    name: 'Columbia'
+  }));
+  campuses.push(Campus.build({
+    name: 'Fordham'
+  }));
+  campuses.push(Campus.build({
+    name: 'Parsons'
+  }));
+  return campuses;
+};
+
+const createStudents = () => {
+  return Promise.map(generateStudents(), student => student.save());
+};
+
+const createCampuses = () => {
+  return Promise.map(generateCampuses(), campus => campus.save());
+};
 
 const sync = () => {
   return conn.sync({ force: true });
 };
 
 const seed = () => {
-  const campusPromises = Promise.all(seedCampus.map(campus => Campus.create(campus)));
-  const studentPromises = Promise.all(seedStudent.map(student => Student.create(student)));
-  return Promise.all([ studentPromises, campusPromises ]);
+  return createStudents()
+  .then(() => createCampuses());
 };
 
 module.exports = {
